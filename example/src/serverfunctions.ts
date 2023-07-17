544109fb4394690ccf05479825467a8c77ea37d1
lilnasy <69170106+lilnasy@users.noreply.github.com>
2023-07-17 20:14:12 +0530

    initial streams support

--- example/src/serverfunctions.ts
+++ example/src/serverfunctions.ts
@@ -1,3 +1,12 @@
 export async function f() {
     return "Hello from server functions!"
 }
+
+export async function sendStreamToServer(readable : ReadableStream<string>) {
+    
+    // @ts-ignore this runs on the server and the servers implement asynIterable for readable streams
+    for await (const value of readable)
+        console.log(value)
+    
+    return "done reading"
+}
