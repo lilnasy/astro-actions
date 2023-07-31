98f2582db2547269cd97c55ddbfe09919d8d2265
lilnasy <69170106+lilnasy@users.noreply.github.com>
2023-07-31 09:34:40 +0530

    migrate to es-codec 0.4

--- es-codec.ts
+++ es-codec.ts
@@ -5,23 +5,23 @@ that has been extended to allow more types.
 This is used by both the client and the server runtime.
 */
 
-import { createCodec } from "es-codec"
+import { defineContext, defineExtension } from "es-codec"
 import iota from "./iota.ts"
 
-export const { encode, decode } = createCodec<WebSocket>([
-    {
+export const { encode, decode } = defineContext<WebSocket>().createCodec([
+    defineExtension({
         name: "URL",
-        when  (x    : unknown) : x is URL { return x?.constructor === URL },
-        encode(url  : URL    ) { return url.href },
-        decode(href : string ) { return new URL(href) }
-    },
-    {
+        when(x) : x is URL { return x?.constructor === URL },
+        encode(url)  { return url.href },
+        decode(href) { return new URL(href) }
+    }),
+    defineExtension({
         name: "RS",
-        when  (x    : unknown) : x is ReadableStream<unknown> { return x?.constructor === ReadableStream },
-        encode(stream : ReadableStream<unknown>, ws) {
+        when(x) : x is ReadableStream { return x?.constructor === ReadableStream },
+        encode(stream, ws) {
             const streamId = iota()
             stream.pipeTo(new WritableStream({
-                write(chunk) {
+                write(chunk : string) {
                     ws.send(encode([ "enqueue", streamId, chunk ], ws))
                 },
                 close() {
@@ -30,44 +30,42 @@ export const { encode, decode } = createCodec<WebSocket>([
             }))
             return streamId
         },
-        decode(streamId : number, ws) {
+        decode(streamId, ws) {
             return new ReadableStream({
-                async start(controller) {
-                    ws.addEventListener("message", listenerForChunks)
-                    function listenerForChunks({ data } : any) {
+                start(controller) {
+                    ws.addEventListener("message", function listenerForChunks({ data } : any) {
                         const [ type, id, value ] = decode(data, ws) as any
                         if (type === "enqueue" && id === streamId) {
                             controller.enqueue(value)
                         }
                         if (type === "close" && id === streamId) {
-                            controller.close();
+                            controller.close()
                             ws.removeEventListener("message", listenerForChunks)
                         }
-                    }
+                    })
                 }
             })
         }
-    },
-    {
+    }),
+    defineExtension({
         name: "WS",
-        when(x : unknown) : x is WritableStream { return x?.constructor === WritableStream },
-        encode(stream : WritableStream<unknown>, ws) {
+        when(x) : x is WritableStream { return x?.constructor === WritableStream },
+        encode(stream, ws) {
             const streamId = iota()
             const writer = stream.getWriter()
-            ws.addEventListener("message", listenerForChunks)
-            return streamId
-            function listenerForChunks({ data } : any) {
+            ws.addEventListener("message", function listenerForChunks({ data } : any) {
                 const [ type, id, value ] = decode(data, ws) as any
                 if (type === "enqueue" && id === streamId) {
                     writer.write(value)
                 }
                 if (type === "close" && id === streamId) {
-                    writer.close();
+                    writer.close()
                     ws.removeEventListener("message", listenerForChunks)
                 }
-            }
+            })
+            return streamId
         },
-        decode(streamId : number, ws) {
+        decode(streamId, ws) {
             return new WritableStream({
                 write(chunk) {
                     ws.send(encode([ "enqueue", streamId, chunk ], ws))
@@ -77,5 +75,5 @@ export const { encode, decode } = createCodec<WebSocket>([
                 }
             })
         }
-    }
+    })
 ])
