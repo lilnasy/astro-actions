7cd361303802256b2bc74e2f4b9666e06a7ca309
lilnasy <69170106+lilnasy@users.noreply.github.com>
2023-08-03 19:14:04 +0530

    support promise, request, response

--- es-codec.ts
+++ es-codec.ts
@@ -11,13 +11,61 @@ import iota from "./iota.ts"
 // @ts-ignore who cares
 const AsyncFunction = (async x => x).constructor
 
+// TODO: each event listener is decoding every message
+// leading to one message being decoded multiple times
+
 export const { encode, decode } = defineContext<WebSocket>().createCodec([
+    // whatwg url
     defineExtension({
         name: "URL",
         when(x) : x is URL { return x?.constructor === URL },
         encode(url)  { return url.href },
         decode(href) { return new URL(href) }
     }),
+    // promise
+    defineExtension({
+        name: "Pr",
+        when(x) : x is Promise<unknown> { return x?.constructor === Promise },
+        encode(promise, ws) {
+            const promiseId = iota()
+            promise
+                .then(value => ws.send(encode([ "promise fulfill", promiseId, value ], ws)))
+                .catch(reason => ws.send(encode([ "promise rejected", promiseId, reason ], ws)))
+            return promiseId
+        },
+        decode(promiseId, ws) {
+            return new Promise((fulfill, reject) => {
+                ws.addEventListener("message", function listenForResolution({ data }) {
+                    const [ type, incomingPromiseId, resolution ] = decode(data, ws) as any
+                    if (type === "promise fulfill" && incomingPromiseId === promiseId) fulfill(resolution)
+                    if (type === "promise rejected" && incomingPromiseId === promiseId) reject(resolution)
+                })
+            })
+        }
+    }),
+    // whatwg request
+    defineExtension({
+        name: "Rq",
+        when(x) : x is Request { return x?.constructor === Request },
+        encode({ body, headers, method, url }) {
+            return { body, url, method, headers: [ ...headers.entries() ] }
+        },
+        decode({ body, headers, method, url }) {
+            return new Request(url, { body , headers, method })
+        }
+    }),
+    // whatwg response
+    defineExtension({
+        name: "Rs",
+        when(x) : x is Response { return x?.constructor === Response },
+        encode({ body, headers, status, statusText }) {
+            return { body, headers: [ ...headers.entries() ], status, statusText }
+        },
+        decode({ body, headers, status, statusText }) {
+            return new Response(body, { headers, status, statusText })
+        }
+    }),
+    // readable streams
     defineExtension({
         name: "RS",
         when(x) : x is ReadableStream { return x?.constructor === ReadableStream },
@@ -50,6 +98,7 @@ export const { encode, decode } = defineContext<WebSocket>().createCodec([
             })
         }
     }),
+    // writable streams
     defineExtension({
         name: "WS",
         when(x) : x is WritableStream { return x?.constructor === WritableStream },
@@ -79,11 +128,10 @@ export const { encode, decode } = defineContext<WebSocket>().createCodec([
             })
         }
     }),
+    // async functions
     defineExtension({
         name: "AF",
-        when(x): x is Function {
-            return x?.constructor === AsyncFunction
-        },
+        when(x): x is Function { return x?.constructor === AsyncFunction },
         encode(fun, ws) {
             const funId = iota()
             ws.addEventListener("message", async function listenForInvocations ({ data }) {
